254b78c98e07aba50427b91279c87e9bf81a691c
Mokarama <mokaramaaktershanta@gmail.com>
2026-01-05 16:01:24 +0600

    added index.js, mark.js, switch.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+
+// let x=prompt("Enter a first number :");
+// let y=prompt("Enter a second number :");
+// let output=x +y;
+// console.log(output);
+
+
+let x=20;
+
+if(x>=20){
+    x=x*5;
+    console.log(x)
+}
+else{
+    console.log(x)
+}
---
+++ mark.js
@@ -0,0 +1,52 @@
+// let mark=70;
+
+// if(mark>=80){
+//     console.log("A+")
+// }
+
+// else if(mark>=70){
+//     console.log("A")
+// }
+
+// else if(mark>=60){
+//     console.log("A-")
+// }
+
+// else if(mark>=50){
+//     console.log("B")
+// }
+
+// else if(mark>=40){
+//     console.log("C")
+// }
+
+// else{
+//     console.log("Fail")
+// }
+
+
+let marks=40;
+
+if(marks>=80 && marks<=90){
+    console.log("A+")
+}
+
+else if(marks>=60 && marks<=70){
+    console.log("A")
+}
+
+else if(marks>=50 && marks<=59){
+    console.log("A-")
+}
+
+else if(marks>=40 && marks<=49){
+    console.log("B")
+}
+
+else if(marks>=33 && marks<=39){
+    console.log("C")
+}
+
+else{
+    console.log("Fail")
+}
