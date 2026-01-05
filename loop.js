3400354f351bb42d8e37c5d7f4cb0885d644367f
Mokarama <mokaramaaktershanta@gmail.com>
2026-01-05 17:24:15 +0600

    added loop.js file

---
+++ loop.js
@@ -0,0 +1,39 @@
+// let a =5;
+
+// if(a%2===0){
+//     console.log("Even number")
+// }
+// else{
+//     console.log("Odd number")
+// }
+
+
+// for(let i=1; i<=20; i++){
+//     if(i% 2== 0){
+//         console.log(i)
+//     }
+// }
+
+
+// for(let x=0; x<=10; x++){
+// let termsOfNum=10*x;
+
+// console.log(`10 * ${x} =${termsOfNum}`)
+// }
+
+
+let y=0;
+while(y<=10){
+    y++;
+
+let termsOfNum1=10*y;
+console.log(termsOfNum1)
+}
+
+
+//
+let n=5;
+for(let i=1; i<=n; i++){
+ let str="*";
+ console.log(str.repeat(i))
+}
--- switch_case.js
+++ switch_case.js
@@ -1,1 +1,18 @@
-let 
+let fruit ="Bananas";
+
+switch(fruit){
+   case "Orange":
+    console.log("Orange---");
+    break;
+
+case "Mango":
+    console.log("Green---");
+    break;
+
+case "Bananas":
+    console.log("Yellow---");
+    break;
+
+ default:
+    console.log('sorry...')
+}
